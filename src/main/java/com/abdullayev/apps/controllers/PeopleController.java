d1282183c2b2d684519a87c9b7ee1a20c3b3a15b
Ramazan <ramazan.abdullayev@bsu.edu.az>
2025-02-24 21:33:27 +0400

    CRUD

--- src/main/java/com/abdullayev/apps/controllers/PeopleController.java
+++ src/main/java/com/abdullayev/apps/controllers/PeopleController.java
@@ -1,13 +1,13 @@
 package com.abdullayev.apps.controllers;
 
 import com.abdullayev.apps.dao.PersonDAO;
+import com.abdullayev.apps.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Objects;
 
 
 @Controller
@@ -31,5 +31,34 @@ public class PeopleController {
         // We will get one person by id from DAO and pass it to display in the view
         model.addAttribute("person", personDAO.show(id));
         return "/people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@PathVariable("id") int id, @ModelAttribute("person") Person person) {
+        personDAO.update(person, id);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        personDAO.delete(id);
+        return "redirect:/people";
     }
 }
