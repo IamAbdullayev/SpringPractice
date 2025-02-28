98bc32ea450bf7b0786388ac310cd0ae0092aa64
Ramazan <ramazan.abdullayev@bsu.edu.az>
2025-02-28 23:55:59 +0400

    3.0

--- src/main/java/com/abdullayev/apps/controllers/PeopleController.java
+++ src/main/java/com/abdullayev/apps/controllers/PeopleController.java
@@ -3,14 +3,10 @@ package com.abdullayev.apps.controllers;
 import com.abdullayev.apps.dao.PersonDAO;
 import com.abdullayev.apps.models.Person;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Objects;
 
 
 @Controller
